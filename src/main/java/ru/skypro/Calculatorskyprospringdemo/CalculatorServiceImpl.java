f1880581f95ccb5d36a1945325662878c1b437fd
vivatgreen <grindsasha@bk.ru>
2022-04-28 02:52:03 +0300

    Калькулятор

---
+++ src/main/java/ru/skypro/Calculatorskyprospringdemo/CalculatorServiceImpl.java
@@ -0,0 +1,33 @@
+package ru.skypro.Calculatorskyprospringdemo;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService{
+    private int totalSum;
+
+
+    @Override
+    public String getPlus(int value1, int value2) {
+        totalSum = value1 + value2;
+        return String.valueOf(totalSum);
+    }
+
+    @Override
+    public String getNegative(int value1, int value2) {
+        totalSum = value1 - value2;
+        return String.valueOf(totalSum);
+    }
+
+    @Override
+    public String getMultiply(int value1, int value2) {
+        totalSum = value1 * value2;
+        return String.valueOf(totalSum);
+    }
+
+    @Override
+    public String getDivide(int value1, int value2) {
+        totalSum = value1 / value2;
+        return String.valueOf(totalSum);
+    }
+}
