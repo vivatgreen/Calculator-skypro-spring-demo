0ff2fc027de13ef606e4ac8dce8c00333de4cf44
vivatgreen <grindsasha@bk.ru>
2022-04-28 02:48:07 +0300

    Коммит

---
+++ src/main/java/ru/skypro/Calculatorskyprospringdemo/CalculatorSkyproSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.Calculatorskyprospringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorSkyproSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorSkyproSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/Calculatorskyprospringdemo/CalculatorSkyproSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.Calculatorskyprospringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorSkyproSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
